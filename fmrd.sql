cb729f839045fde458035b456f14ba8ca46dee23
Howard Hamilton <hhamilton@soccermetrics.net>
2011-08-19 23:58:14 -0400

    Added match_attendance field to Matches table (Closed #7)

--- fmrd.sql
+++ fmrd.sql
@@ -167,6 +167,7 @@ CREATE TABLE tbl_matches (
 	match_date						date,
 	match_firsthalftime	 	integer DEFAULT 45 CHECK (match_firsthalftime > 0),
 	match_secondhalftime 	integer DEFAULT 45 CHECK (match_secondhalftime >= 0),
+	match_attendance			integer DEFAULT 0 CHECK (match_attendance >= 0),
 	competition_id				integer REFERENCES tbl_competitions,
 	round_id							integer REFERENCES tbl_rounds,
 	venue_id							integer REFERENCES tbl_venues,
