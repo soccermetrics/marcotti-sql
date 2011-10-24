9a2487a48496fa15a36ae653f53908907fcb6b8f
Howard Hamilton <hhamilton@soccermetrics.net>
2011-10-24 19:05:44 -0400

    Added extra time fields to Matches table (Closed #26)

--- fmrd.sql
+++ fmrd.sql
@@ -195,6 +195,8 @@ CREATE TABLE tbl_matches (
 	match_date				date,
 	match_firsthalftime	 	integer DEFAULT 45 CHECK (match_firsthalftime > 0),
 	match_secondhalftime 	integer DEFAULT 45 CHECK (match_secondhalftime >= 0),
+	match_firstovertime     integer DEFAULT 0 CHECK (match_firstovertime >= 0),
+	match_secondovertime    integer DEFAULT 0 CHECK (match_secondovertime >= 0),
 	match_attendance		integer DEFAULT 0 CHECK (match_attendance >= 0),
 	competition_id			integer REFERENCES tbl_competitions,
 	venue_id				integer REFERENCES tbl_venues,
